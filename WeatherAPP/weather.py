05affaa38a0a7e351f7f6d9d7bf84ff0b5ba3534
Nyck Bastos <90692553+TrInfra@users.noreply.github.com>
2024-06-25 19:17:09 -0300

    First Commit

---
+++ WeatherAPP/weather.py
@@ -0,0 +1,19 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+
+
+# Variaveis dos backgrounds
+# background_Day = "Weather-Forecasting-System/evidences/imgs/6.png"
+# background_rain ="Weather-Forecasting-System/evidences/imgs/10.png"
+# background_night ="Weather-Forecasting-System/evidences/imgs/8.png"
+# background_NightRain ="Weather-Forecasting-System/evidences/imgs/7.png"
+# background_Evening ="Weather-Forecasting-System/evidences/imgs/9.png"
+
+# Inicializando a janela principal
+window = Tk()
+window.title("Weather App")
+window.geometry("402x402")
+
+
