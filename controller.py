9556963cb14b747cfe974086003a43aabda8171f
leh826 <dev.rocha826@gmail.com>
2024-06-26 17:17:11 -0300

    update: deletado arquivo controller

--- controller.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-chave= '1bebbc645d7db8fbf9319e8735fe94d1'
-api_link = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={}'.format(chave)
-
-#fazendo chamada da API usando request
